d4e8dd7dcdb59f6b241eeab868c9a4b9a8100c9c
Michael Clermont <michael.clermont@mths.ca>
2022-02-25 19:45:41 +0000

    edited areaAndperimeter program

--- areaAndperimeter.cpp
+++ areaAndperimeter.cpp
@@ -11,8 +11,8 @@
 int main() {
     std::cout << "If a circle has a radius of 15 mm: " << std::endl;
     std::cout << std::endl;
-    std::cout << "Area is " << (M_PI * pow(15, 2)) << "cm²." << std::endl;
-    std::cout << "Perimeter is " << (2 * M_PI * 15) << "cm." << std::endl;
+    std::cout << "Area is " << (M_PI * pow(15, 2)) << " mm²." << std::endl;
+    std::cout << "Perimeter is " << (2 * M_PI * 15) << " mm." << std::endl;
     std::cout << std::endl;
     std::cout << "Done." << std::endl;
 }
